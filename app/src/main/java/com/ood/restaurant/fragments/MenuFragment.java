b5490aa107d42918f01e53d1e3531a1225f3cdee
DSLewis22 <dl01852@georgiasouthern.edu>
2016-04-05 17:33:31 -0400

    added RecyclerView for MenuItems. Still need to show when Button is clicked.

--- app/src/main/java/com/ood/restaurant/fragments/MenuFragment.java
+++ app/src/main/java/com/ood/restaurant/fragments/MenuFragment.java
@@ -7,7 +7,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
+
 import com.ood.restaurant.MenuItemData;
 import com.ood.restaurant.MenuItemViewAdapter;
 import com.ood.restaurant.R;
