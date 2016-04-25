97e1e894ae4ee20c6b34939bbfe0a3dd28e73c93
dl01852 <davlewis2011@gmail.com>
2016-04-25 19:53:03 -0400

    added order object and an order database to hold info about an order for a particular table.

--- app/src/main/java/com/ood/restaurant/Order.java
+++ app/src/main/java/com/ood/restaurant/Order.java
@@ -10,9 +10,10 @@ import com.ood.restaurant.Data.Food;
 public class Order {
 
     private int id;
-    String orderDescription;
-    Food food;
+    private String orderDescription;
+    private Food food;
 
+    // An assload of constructors cause i don't know which ones i'll need yet lol
     public Order(int id,Food food,String orderDescription)
     {
         this.id = id;
@@ -26,10 +27,23 @@ public class Order {
         this.orderDescription = orderDescription;
     }
 
-    public Order(int)
+    public Order(int id, Food food)
     {
-
+        this.id = id;
+        this.food = food;
+        this.orderDescription = food.getDescription();
     }
+    public Order(){}
 
+
+    // Setters
+    public void setID(int id){this.id = id;}
+    public void setOrderDescription(String orderDescription){this.orderDescription = orderDescription;}
+    public void setFood(Food food){this.food = food;}
+
+    //getters
+    public int getId(){return this.id;}
+    public String getOrderDescription(){return this.orderDescription;}
+    public Food getFood(){return this.food;}
 
 }
--- app/src/main/java/com/ood/restaurant/orderDatabase.java
+++ app/src/main/java/com/ood/restaurant/orderDatabase.java
@@ -1,21 +1,25 @@
 package com.ood.restaurant;
 import android.content.ContentValues;
 import android.content.Context;
-import android.database.DatabaseUtils;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.support.design.widget.TabLayout;
 
 
 /**
  * Created by david-lewis on 4/25/2016.
+ *
+ * Quick mockup of an a database...
  */
 public class orderDatabase extends SQLiteOpenHelper {
 
     public static final String DATABASE_NAME = "Restaurant.db";
     public static final String TABLE_NAME = "orders";
-    public static final String TABLE_ID = "id";
+    public static final String ORDER_ID = "id";
     public static final String ORDER = "order";
+
+    private int id;
+    private String orderDescription;
 
 
     public orderDatabase(Context context)
@@ -26,22 +30,85 @@ public class orderDatabase extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db)
     {
-        StringBuilder query = new StringBuilder();
-        query.append(String.format("CREATE TABLE %s\n",TABLE_NAME));
-        query.append("(");
-        query.append(String.format("%s INTEGER PRIMARY KEY,\n",TABLE_ID));
-        query.append(String.format("%s text\n",ORDER));
-        query.append(")");
+        // Create the query to create a table in the database..
+        String query = String.format("CREATE TABLE %s\n", TABLE_NAME) +
+                "(" +
+                String.format("%s INTEGER PRIMARY KEY,\n", ORDER_ID) +
+                String.format("%s text\n", ORDER) +
+                ")";
 
-        db.execSQL(query.toString());
+        db.execSQL(query);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
+        // function is mandatory.. we'll have no use for it though.
         db.execSQL(String.format("DROP TABLE IF EXISTS %s",TABLE_NAME));
         onCreate(db);
     }
 
-    public boolean insertOrder()
+    public boolean insertOrder(Order order)
+    {
+        // inserting an order into the database.
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+
+        contentValues.put(ORDER,order.getOrderDescription());
+        db.insert(TABLE_NAME,null,contentValues);
+        db.close();
+
+        return true;
+    }
+
+    public boolean updateOrder(Order order)
+    {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+
+        contentValues.put(ORDER,order.getOrderDescription());
+
+        db.update(TABLE_NAME,contentValues,"id = ?",new String[]{Integer.toString(order.getId())});
+        return true;
+    }
+
+    public Order getOrder(int id) {
+        Order order; // order to be returned.
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursorData = db.rawQuery("SELECT * FROM orders WHERE id = ?", new String[]{String.valueOf(id)});
+
+        if (cursorData.moveToFirst()) {
+            this.id = cursorData.getInt(cursorData.getColumnIndex(ORDER_ID));
+            this.orderDescription = cursorData.getString(cursorData.getColumnIndex(ORDER));
+            cursorData.close();
+
+
+            order = new Order(id, orderDescription);
+            db.close();
+            return order;
+        } else
+            return null; // Error
+    }
+
+    public Integer deleteOrder(Order order)
+    {
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.delete(TABLE_NAME,"WHERE id = ?",new String[]{Integer.toString(order.getId())});
+    }
+
+    public void deleteAllOrders()
+    {
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(String.format("SELECT * FROM %s",TABLE_NAME),null);
+
+        while(cursor.moveToNext())
+        {
+            db.delete(TABLE_NAME,"id = ?", new String[]{Integer.toString(cursor.getInt(cursor.getColumnIndex(ORDER_ID)))});
+        }
+
+        cursor.close();
+        db.close();
+    }
+
+
 }
