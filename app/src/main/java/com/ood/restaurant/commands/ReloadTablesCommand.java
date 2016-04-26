5ac55451b6a6bc5bdfad35cbc998e63b80f62dcf
Michael Palmer <mp03894@georgiasouthern.edu>
2016-04-26 15:22:31 -0400

    Removing the navigation drawer since we don't really need it

--- app/src/main/java/com/ood/restaurant/commands/ReloadTablesCommand.java
+++ app/src/main/java/com/ood/restaurant/commands/ReloadTablesCommand.java
@@ -2,7 +2,17 @@ package com.ood.restaurant.commands;
 
 import com.ood.restaurant.fragments.TableFragment;
 
+/**
+ * This command will reload all of the tables on the main screen. It is executed after both the
+ * MakeAvailableCommand and SeatTableCommand classes.
+ *
+ * @author Michael Palmer
+ */
 public class ReloadTablesCommand {
+    /**
+     * Invalidate all views in the GridView and reload the adapter. This will change the color
+     * of a table when it has been seating or made available.
+     */
     public static void execute() {
         TableFragment.tableGrid.invalidateViews();
         TableFragment.tableGrid.setAdapter(TableFragment.tableGrid.getAdapter());
