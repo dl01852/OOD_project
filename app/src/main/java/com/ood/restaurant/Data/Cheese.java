a721d81a1bf059f8e0bd2cc4e37ad9522fca41d9
Michael Palmer <mp03894@georgiasouthern.edu>
2016-04-26 22:25:08 -0400

    Starting on facade

--- app/src/main/java/com/ood/restaurant/Data/Cheese.java
+++ app/src/main/java/com/ood/restaurant/Data/Cheese.java
@@ -4,14 +4,14 @@ package com.ood.restaurant.Data;
  */
 public class Cheese extends Decorator
 {
-    Food burger;
+    Food food;
     String name = "Cheese";
     double cost = .25;
     String decoratorDescription = "cheddar cheese";
 
-    public Cheese(Food burgerToOrder)
+    public Cheese(Food foodToOrder)
     {
-        this.burger = burgerToOrder;
+        this.food = foodToOrder;
     }
 
     public Cheese(){}
@@ -20,8 +20,8 @@ public class Cheese extends Decorator
     public String getDescription()
     {
 
-        return burger.getDescription() + " with cheese ";
-        // get the description from the burger object and then add on the description from this class
+        return food.getDescription() + " with cheese ";
+        // get the description from the food object and then add on the description from this class
         // soo for this particular class, the description you'd add is  ' with lettuce'
         // then return that(don't return null).
     }
@@ -45,8 +45,8 @@ public class Cheese extends Decorator
     public double cost()
     {
         //return .25;
-        return burger.cost() + cost;
-        // get the cost from the burger object and then add on the cost for lettuce(if there is a cost) and return that value.
+        return food.cost() + cost;
+        // get the cost from the food object and then add on the cost for lettuce(if there is a cost) and return that value.
         // (don't return 0)
     }
 
--- app/src/main/java/com/ood/restaurant/Listeners.java
+++ app/src/main/java/com/ood/restaurant/Listeners.java
@@ -3,6 +3,5 @@ package com.ood.restaurant;
 public class Listeners {
     public interface OnCustomizeListener {
         void onCustomizeClicked(String itemName);
-        void onOrderClicked();
     }
 }
