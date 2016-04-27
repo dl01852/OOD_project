f03a3be5ec5ec8676ea7c7377f5be8adc0648467
Archer <jarcher1200@gmail.com>
2016-04-27 12:45:47 -0400

    Added back button for orders

--- app/src/main/java/com/ood/restaurant/MainActivity.java
+++ app/src/main/java/com/ood/restaurant/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
+import android.view.View;
 import com.ood.restaurant.fragments.TableFragment;
 
 public class MainActivity extends AppCompatActivity {
@@ -28,6 +29,19 @@ public class MainActivity extends AppCompatActivity {
         // Configure the toolbar
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.fragment_layout, new TableFragment())
+                        .commit();
+            }
+        });
 
         // Start the TableFragment
         getSupportFragmentManager()
