e17e886f2c61e018a6483b8fca39ceb387144b78
Michael Palmer <mp03894@georgiasouthern.edu>
2016-04-28 19:37:07 -0400

    Update unit tests

--- app/src/androidTest/java/com/ood/restaurant/OrderTest.java
+++ app/src/androidTest/java/com/ood/restaurant/OrderTest.java
@@ -1,29 +1,28 @@
 package com.ood.restaurant;
 
+import android.test.AndroidTestCase;
 import com.ood.restaurant.Data.Burger;
-import com.ood.restaurant.Data.Food;
-import junit.framework.TestCase;
 
 /**
  * Created by Archer on 4/27/16.
  */
-public class OrderTest extends TestCase {
+public class OrderTest extends AndroidTestCase {
     public void testID() throws Exception {
         Order order = new Order();
         order.setID(1);
-        assertEquals(true, order.getId() == 1);
+        assertEquals(order.getId(), 1);
     }
 
     public void testDescription() throws Exception {
         Order order = new Order();
         order.setOrderDescription("Pizza");
-        assertEquals(true, order.getOrderDescription().equals("Pizza"));
+        assertEquals(order.getOrderDescription(), "Pizza");
     }
 
     public void testFood() throws Exception {
         Order order = new Order();
         Burger burger = new Burger();
         order.setFood(burger);
-        assertEquals(true, order.getFood() == burger);
+        assertEquals(order.getFood(), burger);
     }
 }
